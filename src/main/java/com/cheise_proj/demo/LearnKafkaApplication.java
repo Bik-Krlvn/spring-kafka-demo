35c4d2c475baa3c116696b3d7a2459f2f098459e
bik_krlvn <kelvinbirikorang@gmail.com>
2020-11-21 12:24:00 +0000

    :tada: initial app setup

---
+++ src/main/java/com/cheise_proj/demo/LearnKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.cheise_proj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnKafkaApplication.class, args);
+    }
+
+}
